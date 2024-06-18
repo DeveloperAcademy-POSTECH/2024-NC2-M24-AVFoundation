ce1a4aea598c0ee879ccda408dc9cdf7674c868b
Xerath <yoondj98@naver.com>
2024-06-18 18:02:45 +0900

    [feat] 이미지 선택 View 구현 및 네비게이션 설정

--- LiveFourCut/LiveFourCut/SceneDelegate.swift
+++ LiveFourCut/LiveFourCut/SceneDelegate.swift
@@ -11,11 +11,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = ViewController()
+        self.window?.rootViewController = UINavigationController(rootViewController: FrameSelectionViewController())
         self.window?.makeKeyAndVisible()
     }
 
